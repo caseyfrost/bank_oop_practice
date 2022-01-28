720c156c59d15dafde85cc99b415806289cc9857
caseyfrost <caseyfrost13@gmail.com>
2022-01-28 08:43:06 -0800

    Created service class, added address to user

---
+++ service.py
@@ -0,0 +1,15 @@
+class Service:
+    """Parent class for all bank services
+
+    Attributes:
+        account_number: str of account number
+        customer_id: str of account owner's customer id
+    """
+
+    def __init__(self, account_number, customer_id):
+        self.account_number = account_number
+        self.customer_id = customer_id
+
+    def bill_customer(self, amount):
+        """After db is set up will send a bill to the customer's address"""
+
--- user.py
+++ user.py
@@ -11,10 +11,11 @@ class User:
         social: property, a string of the last 4 digits of the user's social. Defaults to empty string.
     """
 
-    def __init__(self, first_name, last_name, date_of_birth, email, social=''):
+    def __init__(self, first_name, last_name, date_of_birth, address, email, social=''):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
+        self.address = address
         self.email = email
         self._social = social
 
